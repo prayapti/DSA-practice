577ac63f08542757513da6ce50fcdbfcb31629d5
Prayapti <prayaptinugrawar@gmail.com>
2025-07-16 17:32:41 +0530

    Created BinarySearch.java

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,16 @@
+// LeetCode 704: Binary Search
+// https://leetcode.com/problems/binary-search/
+// Difficulty: Easy
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return -1;
+    }
+}
