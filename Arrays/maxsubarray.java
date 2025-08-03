37dc8cd4117c0d99f95edc540f967f37d38e99c3
Prayapti <prayaptinugrawar@gmail.com>
2025-08-03 21:58:00 +0530

    maxsubarray.java

---
+++ Arrays/maxsubarray.java
@@ -0,0 +1,19 @@
+// Kadane's Algorithm – Java Implementation
+//Difficulty: Medium
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            maxSum = Math.max(maxSum, currSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
