d16ddb06ff70873727f46dbcdce42fbe150b4d70
Prayapti <prayaptinugrawar@gmail.com>
2025-08-18 19:24:48 +0530

    Updated spiral.java

--- Arrays/spiral.java
+++ Arrays/spiral.java
@@ -1,51 +1,47 @@
 //leetcode Problem No.: 54
 //Difficulty: 🟠 Medium
 
-public class SpiralMatrix {
-    // Print spiral order
-    public static void printSpiral(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            System.out.println("empty matrix");
-            return;
-        }
+import java.util.*;
 
-        int startRow = 0, endRow = matrix.length - 1;
-        int startCol = 0, endCol = matrix[0].length - 1;
+class Solution {
+    //List<Integer> ka matlab hai ki function ek List return karega jisme Integers store honge.
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
 
-        StringBuilder sb = new StringBuilder();
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
 
         while (startRow <= endRow && startCol <= endCol) {
-            // top row
-            for (int j = startCol; j <= endCol; j++) sb.append(matrix[startRow][j]).append(' ');
-
-            // right col
-            for (int i = startRow + 1; i <= endRow; i++) sb.append(matrix[i][endCol]).append(' ');
-
-            // bottom row
-            if (startRow < endRow) {
-                for (int j = endCol - 1; j >= startCol; j--) sb.append(matrix[endRow][j]).append(' ');
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                result.add(matrix[startRow][j]);
             }
 
-            // left col
-            if (startCol < endCol) {
-                for (int i = endRow - 1; i >= startRow + 1; i--) sb.append(matrix[i][startCol]).append(' ');
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                result.add(matrix[i][endCol]);
             }
 
-            startRow++; endRow--;
-            startCol++; endCol--;
+            // bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startRow == endRow) break; 
+                result.add(matrix[endRow][j]);
+            }
+
+            // left
+            for (int i = endRow - 1; i > startRow; i--) {
+                if (startCol == endCol) break;
+                result.add(matrix[i][startCol]);
+            }
+
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
         }
 
-        System.out.println(sb.toString().trim());
-    }
-
-    public static void main(String[] args) {
-        // fixed input matrix
-        int[][] matrix = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12}
-        };
-
-        printSpiral(matrix);
+        return result;
     }
 }
